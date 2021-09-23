7f54c02b448589a7238465f1ce4eb66c7cfe720c
Caleb Elliott <calebpe531@gmail.com>
2021-09-23 15:42:37 -0400

    Finishes 4.3

--- HW1/F21_HW1/test.py
+++ HW1/F21_HW1/test.py
@@ -4,11 +4,172 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import ks_2samp
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
+from sklearn.decomposition import PCA
 
-a = np.array([[1,2,3],[4,5,6]]) 
+
+np.random.seed(2342)
+mu_vec1 = np.array([0,0,0])
+cov_mat1 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class1_sample = np.random.multivariate_normal(mu_vec1, cov_mat1, 30).T
+assert class1_sample.shape == (3,30), "The matrix has not the dimensions 3x30"
+
+mu_vec2 = np.array([1,1,1])
+cov_mat2 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class2_sample = np.random.multivariate_normal(mu_vec2, cov_mat2, 30).T
+assert class2_sample.shape == (3,30), "The matrix has not the dimensions 3x30"
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, projection='3d')
+plt.rcParams['legend.fontsize'] = 10   
+ax.plot(class1_sample[0,:], class1_sample[1,:], class1_sample[2,:], 'o', markersize=8, color='blue', alpha=0.5, label='class1')
+ax.plot(class2_sample[0,:], class2_sample[1,:], class2_sample[2,:], '^', markersize=8, alpha=0.5, color='red', label='class2')
+
+plt.title('Samples for class 1 and class 2')
+ax.legend(loc='upper right')
+
+#plt.show()
+
+#1
+allx = np.append(class1_sample[0,:],  class2_sample[0,:], 0)
+ally = np.append(class1_sample[1,:],  class2_sample[1,:], 0)
+allz = np.append(class1_sample[2,:],  class2_sample[2,:], 0)
+t_all_samples = np.concatenate((class1_sample, class2_sample), axis=1)
+all_samples = np.column_stack((allx, ally, allz))
+
+t_mean_x = np.mean(t_all_samples[0,:])
+t_mean_y = np.mean(t_all_samples[1,:])
+t_mean_z = np.mean(t_all_samples[2,:])
+
+#2
+mean_x = np.mean(all_samples[:,0])
+mean_y = np.mean(all_samples[:,1])
+mean_z = np.mean(all_samples[:,2])
+mean_vector = [[mean_x], [mean_y], [mean_z]]
+mean_vector = np.array(mean_vector)
+t_mean_vector = np.array([[mean_x], [mean_y], [mean_z]])
 
 
-a = np.append(a, [[7,8,9]],0) 
+#3
+t_sm = np.zeros((3,3))
+for i in range(t_all_samples.shape[1]):
+    t_sm += (t_all_samples[:,i].reshape(3,1) - t_mean_vector).dot((t_all_samples[:,i].reshape(3,1) - t_mean_vector).T)
+    s1 = t_all_samples[:,i].reshape(3,1)
+    s1 = s1 - t_mean_vector
+    s2 = s1.dot((s1).T)
+    sf = (t_all_samples[:,i].reshape(3,1) - t_mean_vector).dot((t_all_samples[:,i].reshape(3,1) - t_mean_vector).T)
+coun = 0
+sm = np.zeros((3,3))
+for i in all_samples:
+    intmd = np.array([[i[0]], [i[1]], [i[2]]])
+    intmd = intmd - mean_vector
+    intmd = intmd.dot((intmd).T)
+    sm = sm + intmd
+
+
+
+
+np.random.seed(2342)
+mu_vec1 = np.array([0,0,0])
+cov_mat1 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class1_sample = np.random.multivariate_normal(mu_vec1, cov_mat1, 30).T
+assert class1_sample.shape == (3,30), "The matrix has not the dimensions 3x30"
+
+mu_vec2 = np.array([1,1,1])
+cov_mat2 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class2_sample = np.random.multivariate_normal(mu_vec2, cov_mat2, 30).T
+assert class2_sample.shape == (3,30), "The matrix has not the dimensions 3x30"
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# load dataset into Pandas DataFrame
+df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+
+features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+# Separating out the features
+x = df.loc[:, features].values
+# Separating out the target
+y = df.loc[:,['target']].values
+x = StandardScaler().fit_transform(x)
+
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(x)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
+finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
+
+
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, projection='3d')
+plt.rcParams['legend.fontsize'] = 10   
+ax.plot(class1_sample[0,:], class1_sample[1,:], class1_sample[2,:], 'o', markersize=8, color='blue', alpha=0.5, label='class1')
+ax.plot(class2_sample[0,:], class2_sample[1,:], class2_sample[2,:], '^', markersize=8, alpha=0.5, color='red', label='class2')
+
+plt.title('Samples for class 1 and class 2')
+ax.legend(loc='upper right')
+
+#plt.show()
+
+allx = np.append(class1_sample[0,:],  class2_sample[0,:], 0)
+ally = np.append(class1_sample[1,:],  class2_sample[1,:], 0)
+allz = np.append(class1_sample[2,:],  class2_sample[2,:], 0)
+
+xyz = np.column_stack((allx, ally, allz))
+df = pd.DataFrame(xyz)
+std = df.loc[:].values
+std = StandardScaler().fit_transform(std)
+print(df.head(5))
+# Standardizing the features
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(std)
+principalDf = pd.DataFrame(data=principalComponents, columns=['p1', 'p2'])
+
+final = ['class1']*class1_sample.shape[1] + ['class2']*class2_sample.shape[1]
+principalDf['target'] = final
+print(principalDf.head(5))
+print(principalDf.tail(5))
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = ['class1', 'class2']
+colors = ['r', 'g']
+for target, color in zip(targets,colors):
+    indicesToKeep = principalDf['target'] == target
+    ax.scatter(principalDf.loc[indicesToKeep, 'p1']
+               , principalDf.loc[indicesToKeep, 'p2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
+
+
+
+
+
+
+
+
+
 
 def calculateCentroids(map):
     for item in map:

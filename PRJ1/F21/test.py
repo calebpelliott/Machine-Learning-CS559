1d786f2162ddce4f9a8d145f02f4e5724a435186
Caleb Elliott <calebpe531@gmail.com>
2021-10-15 14:49:37 -0400

    Finishes PRJ1

--- PRJ1/F21/test.py
+++ PRJ1/F21/test.py
@@ -17,8 +17,104 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
+def calcK(guess):
+    count = 0
+    class_guesses = []
+    for _ in range(6):
+        class_guesses.append(guess[count : count + 1250])
+        count += 1250
+
+    tright = 0
+    for cg in class_guesses:
+        cg = cg.tolist()
+        mode = max(set(cg), key=cg.count)
+        right = cg.count(mode)
+        tright += right
+        print(f"Group found: {mode}")
+    k = tright / 7500 * 100
+    print(f"Accuracy {k}%")
+    return k
+
+def plot_3D(df, comp1,comp2,comp3,target):
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(df[comp1],df[comp2],df[comp3],c=df[target])
+    ax.set_xlabel(comp1)
+    ax.set_ylabel(comp2)
+    ax.set_zlabel(comp3)
+    plt.show()
+
+
+
 df = pd.read_csv("C:/Users/caleb/Documents/git/Machine-Learning-CS559/PRJ1/F21/H.csv")
 
+
+
+
+
+
+new_df = df[['var1', 'var2', 'var3', 'var4','var5', 'var6', 'var7', 'var8', 'var9']]
+#new_df = df[['var1', 'var2', 'var3']]
+std = new_df.loc[:].values
+std = StandardScaler().fit_transform(std)
+
+pca = PCA(n_components=3)
+principalComponents = pca.fit_transform(std)
+#principalDf = pd.DataFrame(data=principalComponents, columns=['p1', 'p2'])
+principalDf = pd.DataFrame(data=principalComponents, columns=['p1', 'p2', 'p3'])
+
+principalDf['target'] = df['Class']
+plot_3D(principalDf,'p1','p2','p3','target')
+print(principalDf.head(5))
+
+
+# fig = plt.figure(figsize = (8,8))
+# ax = fig.add_subplot(1,1,1) 
+# ax.set_xlabel('Principal Component 1', fontsize = 15)
+# ax.set_ylabel('Principal Component 2', fontsize = 15)
+# ax.set_title('2 component sklearn PCA', fontsize = 20)
+# targets = [1, 2, 3, 4, 5, 6]
+# colors = ['r', 'g', 'b', 'y', 'm', 'c']
+# for target, color in zip(targets,colors):
+#     indicesToKeep = principalDf['target'] == target
+#     ax.scatter(principalDf.loc[indicesToKeep, 'p1']
+#                , principalDf.loc[indicesToKeep, 'p2']
+#                , c = color
+#                , s = 50)
+# ax.legend(targets)
+# ax.grid()
+# plt.show()
+
+kmeans = KMeans()
+def plot_inertia(km, X, n_cluster_range):
+    inertias = []
+    for i in n_cluster_range:
+        km.set_params(n_clusters=i)
+        km.fit(X)
+        inertias.append(km.inertia_)
+    plt.plot(n_cluster_range, inertias, marker='o')
+    plt.title('Elbow method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
+    plt.show()
+    
+    tabular_result = pd.DataFrame(inertias, 
+                                  index = ["n = {}".format(i) for i in range(1, len(inertias)+1)], 
+                                  columns=['Inertia'])
+    
+    return tabular_result
+
+#new_df = principalDf[['p1', 'p2']]
+new_df = principalDf[['p1', 'p2', 'p3']]
+plot_inertia(kmeans, new_df, range(1, 10))
+
+kmeans = KMeans()
+kmeans.set_params(n_clusters=6)
+kmeans.fit(new_df)
+
+km_guess = kmeans.predict(new_df)
+
+p = calcK(km_guess)
 
 nn = pd.get_dummies(df.Class, columns=['c1', 'c2', 'c3', 'c4', 'c5', 'c6'])
 #nn = nn.rename(columns={'c1', 'c2', 'c3', 'c4', 'c5', 'c6'})
@@ -65,23 +161,7 @@ result_df['knn'] = knn_guess
 
 
 
-def calcK(guess):
-    count = 0
-    class_guesses = []
-    for _ in range(6):
-        class_guesses.append(guess[count : count + 1250])
-        count += 1250
 
-    tright = 0
-    for cg in class_guesses:
-        cg = cg.tolist()
-        mode = max(set(cg), key=cg.count)
-        right = cg.count(mode)
-        tright += right
-        print(f"Group found: {mode}")
-    k = tright / 7500 * 100
-    print(f"Accuracy {k}%")
-    return k
 
 accuracy = []
 below95 = False

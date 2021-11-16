c767dbbb612447be38a2ca13aa45b75db8ee2f2f
Caleb Elliott <calebpe531@gmail.com>
2021-11-15 21:39:34 -0500

    Finishes gradient descent to the best of my ability

--- PRJ2/Project2_F21/Project2_F21/prj2.py
+++ PRJ2/Project2_F21/Project2_F21/prj2.py
@@ -5,16 +5,16 @@ from sklearn.metrics import mean_squared_error
 df = pd.read_csv('./PRJ2/Project2_F21/Project2_F21/F21_CS559_Project2.csv')
 
 def my_grad(X,Y,Z):
-    eta=0.1 #.01,.001
+    eta=0.1#0.1 #.01,.001
     #a
-    c = 1#0.0#1#0.0
+    c = 0#0.0#1#0.0
     #b
     ckn = 0.0
     #c
     ckm = 0.0
     #f
     ck = 0.0
-    gradient_w1=0; gradient_w2=0
+
     gradient_c=0
     gradient_ckn=0
     gradient_ckm=0
@@ -42,16 +42,6 @@ def my_grad(X,Y,Z):
         partial_ck += -2*np.exp(-1*((ck+Y)**2))*(ck+Y)
         partial_ck *= -c
         gradient_ck = np.mean(-(partial_ck*error))
-
-        #gradient_ckn = np.mean(-(e11*error))
-        #gradient_ckm = np.mean(-(e12*error))
-        #gradient_c = np.mean(-(z_pred*error))
-        #gradient_w1 = np.mean(-X**2*error)
-        #gradient_w2 = np.mean(-X*error)
-
-        #update constants
-        #w1 = w1-eta*gradient_w1
-        #w2 = w2-eta*gradient_w2
         
         c = c-eta*gradient_c
         ckn = ckn-eta*gradient_ckn
@@ -61,11 +51,10 @@ def my_grad(X,Y,Z):
         new_mse = np.mean(error**2)/2
         rmse = mean_squared_error(Z, z_pred, squared=False)
         if mse<=0.05:
-            #print(w1,w2,np.mean(error**2)/2)
             break
     return [c, ck, ckn, ckm, mse]
 
-for k in [0,1,2,3]:#[3,2,1,0]:
+for k in [0,1,2,3]:
     # Train the model using the training sets
     kdf = df.loc[df['class'] == k]
     [c, ck, ckn, ckm, mse] = my_grad(kdf['x'],kdf['y'],kdf['z'])

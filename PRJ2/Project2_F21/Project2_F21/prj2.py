76ff84cd7c29c56e8ffe123b44469387a429b083
Caleb Elliott <calebpe531@gmail.com>
2021-11-15 19:19:59 -0500

    Adds partial derivative of ck term

--- PRJ2/Project2_F21/Project2_F21/prj2.py
+++ PRJ2/Project2_F21/Project2_F21/prj2.py
@@ -2,19 +2,24 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv('F21_CS559_Project2.csv')
+df = pd.read_csv('./PRJ2/Project2_F21/Project2_F21/F21_CS559_Project2.csv')
 
 def my_grad(X,Y,Z):
     eta=0.1 #.01,.001
-    c = 0.01
+    #a
+    c = 1#0.0
+    #b
     ckn = 0.0
+    #c
     ckm = 0.0
+    #f
     ck = 0.0
     gradient_w1=0; gradient_w2=0
     gradient_c=0
     gradient_ckn=0
     gradient_ckm=0
     gradient_ck=0
+    mse = 100
     for i in range(0,10000):
         #y = w1*X**2+w2*X
         e1 = np.exp(-((X+ckn)**2 + (Y+ckm)**2))
@@ -28,10 +33,14 @@ def my_grad(X,Y,Z):
         e2 = np.exp(-((X+ck)**2))
         e3 = np.exp(-((Y+ck)**2))
         z_pred = -c*(e1 + e2 + e3)
+        #z_pred = e2 + e3
         error = (Z-z_pred)
 
         #handle gradients
-        gradient_ck = np.mean(-((e2+e3)*error))
+        partial_ck = 2*np.exp(-1*((ck+X)**2))*(ck+X)
+        partial_ck += 2*np.exp(-1*((ck+Y)**2))*(ck+Y)
+        gradient_ck = np.mean(-(partial_ck*error))
+
         gradient_ckn = np.mean(-(e11*error))
         gradient_ckm = np.mean(-(e12*error))
         gradient_c = np.mean(-(z_pred*error))
@@ -41,17 +50,19 @@ def my_grad(X,Y,Z):
         #update constants
         #w1 = w1-eta*gradient_w1
         #w2 = w2-eta*gradient_w2
-        c = c-eta*gradient_c
-        ckn = ckn-eta*gradient_ckn
-        ckm = ckm-eta*gradient_ckm
+        
+        #c = c-eta*gradient_c
+        #ckn = ckn-eta*gradient_ckn
+        #ckm = ckm-eta*gradient_ckm
         ck = ck-eta*gradient_ck
         mse = mean_squared_error(Z, z_pred)
         if mse<=0.05:
             #print(w1,w2,np.mean(error**2)/2)
             break
-    return [c, ck, ckn, ckm]
+    return [c, ck, ckn, ckm, mse]
 
 for k in [0,1,2,3]:
     # Train the model using the training sets
     kdf = df.loc[df['class'] == k]
-    my_grad(kdf['x'],kdf['y'],kdf['z'])
+    [c, ck, ckn, ckm, mse] = my_grad(kdf['x'],kdf['y'],kdf['z'])
+    print(f"Class: {k}\n  C:{c}\n  Ck:{ck}\n  Ckn:{ckn}\n  Ckm:{ckm}\n  MSE:{mse}\n\n")
